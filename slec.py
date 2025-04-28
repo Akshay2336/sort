d7ba63a9bd24ea7851c5609b85811931f3217444
Akshay2336 <akshaykadam2336@gmail.com>
2025-04-28 14:23:58 +0530

    Update slec.py

--- slec.py
+++ slec.py
@@ -3,7 +3,7 @@ def selection_sort(arr):
   for i in range(len(arr)):
     min_ind = i
     for j in range(1+i , (len(arr))):
-      if arr[j < arr[min_ind]
+      if arr[j] < arr[min_ind]
         min_ind = j
       arr[i] , arr[min_ind] = arr[min_ind] , arr[j]
 
